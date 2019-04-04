0e014840af2ae539987ecbc62821295ac1bb057d
jjansa <jpjansa@gmail.com>
2019-04-04 10:27:36 -0500

    nothing to commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import  Flask, render_template, request
+
+
+app = Flask(__name__)
+app.config['DEBUG'] =True
+
+@app.route('/')
+def index():
+    return render_template('index.html', title='Home')
+
+@app.route('/blog.html')
+def blog():
+    return render_template('blog.html', title="Blog")
+
+if (__name__) == '__main__':
+    app.run()
