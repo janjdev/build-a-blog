e2205bf716d0b7b62ed588dde298543d84f0ef33
jjansa <jpjansa@gmail.com>
2019-04-24 22:45:52 -0500

    created all views and client sided functions for the post rich text editor.

--- static/admin/assets/dash/js/actions.js
+++ static/admin/assets/dash/js/actions.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
+  //initalize all tooltips
+    // $('[data-toggle="tooltip"]').tooltip()
     $('.nav-mobile-menu .nav-item a.nav-link').removeClass('btn', 'btn-white', 'btn-round');
   window.onresize = navLink;
   
+  // create mobile view for the navbar
   function navLink(){
     if(window.innerWidth < 991){
       $('.nav-mobile-menu .nav-item a.nav-link').removeClass('btn', 'btn-white', 'btn-round');
     }
   }
-
+  
+  //initate date picker for event posts
   $('.datetimepicker').datetimepicker({
     icons: {
         time: "fa fa-clock-o",
@@ -24,5 +27,58 @@ $(function () {
     }
  });
 
+ //Set cursor focus of contenteditable elements
+    //remove place holder on focus
+    const titlebox = document.querySelector('#addPostTitle');
+    const contentbox = document.querySelector('#contentbox');
+    let s = window.getSelection(), r = document.createRange();
+    const toolsbar = document.querySelector('#toolbar');
+
+    r.setStart(titlebox, 0);
+    r.setEnd(titlebox, 0);
+    s.removeAllRanges();
+    s.addRange(r);
+ 
+  // Create WYSIWYG functions for rich text editors
+  $('.tools a').mousedown(function(e){
+    e.preventDefault();
+    let command = $(this).data('command');
+    if(command == 'bold' || command == 'underline' || command == 'strikeThrough' || command == 'superscript'){
+      $(this).parent().toggleClass('active');
+    }
+    if(command == 'foreColor' || command == 'insertImage' || command == 'heading' || command == 'fontSize' || command == 'fontName'){
+      // let $this = $(this)
+      $(this).next().children('input').trigger('click');
+      let input = $(this).next().children();
+      console.log(input);      
+     input.on('change', function(e){
+        let val = $(input).val();
+        console.log(val);
+        document.execCommand(command, false, val);
+      })
+    }
+    if (command == 'h1' || command == 'h2' || command == 'p') {
+      document.execCommand('formatBlock', false, command);
+    }
+    // if (command == 'foreColor' || command == 'backcolor') {
+    //   document.execCommand($(this).data('command'), false, $(this).data('value'));
+    //   console.log($(this).children('input').val());
+      
+    // }
+    if (command == 'createlink') {
+      url = prompt('Enter the link here: ', 'http:\/\/');
+      document.execCommand($(this).data('command'), false, url);
+    } 
+    else document.execCommand($(this).data('command'), false, null);
+   
+    });
+
+
+
+  
+   
+
+
   });
+
 });
